b8f9b0536bd8727fd37730b784a81c0bcf67b5bd
Brayan L <brayanlujan.mzu@hotmail.com>
2022-04-26 18:45:25 -0500

    Upload the base files for the correct work of the db

---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "ptmabolspxeeuw"); // Usuario
+define('DB_PASSWORD', "204eee85159b4ef2d6adab12dba3210f24bc0b58b798eb7f9c7bc6f12882c98a"); // Password
+define('DB_DATABASE', "d11tikraba3f9p"); // Nombre de la base de datos
+define('DB_SERVER', "ec2-3-218-171-44.compute-1.amazonaws.com"); // host server
+?>
