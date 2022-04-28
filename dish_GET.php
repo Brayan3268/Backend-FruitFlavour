e15139679371f6f862bfd8fdd3548ace3507259e
Brayan L <brayanlujan.mzu@hotmail.com>
2022-04-28 00:40:32 -0500

    Added the page for get all dish v4

--- dish_GET.php
+++ dish_GET.php
@@ -3,21 +3,20 @@
 
 	if($_SERVER['REQUEST_METHOD']=='POST'){
 		try{
-			$datos = json_decode(file_get_contents("php://input"),true);		
-			$respuesta = SQLGlobal::selectArrayFiltro(
-				"select * from dish"
-			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+			//$datos = json_decode(file_get_contents("php://input"),true);
+
+			$respuesta = SQLGlobal::selectArray("select * from dish");
             if($respuesta){
                 echo json_encode(array(
                     'respuesta'=>'200',
-                    'estado' => 'Se obtuvieron correctamente las listas de datos',
+                    'estado' => 'Se obtuvierón los datos correctamente',
                     'data'=>$respuesta,
                     'error'=>''
                 ));
             }else{
                 echo json_encode(array(
                     'respuesta'=>'100',
-                    'estado' => 'No fue posible obtener las listas de datos',
+                    'estado' => 'No existen estos datos',
                     'error'=>''
                 ));
             }
@@ -25,11 +24,10 @@
 			echo json_encode(
 				array(
 					'respuesta'=>'-1',
-					'estado' => 'Ocurrio un error, intentelo más tarde',
+					'estado' => 'Ocurrio un error, intentelo mas tarde',
 					'data'=>'',
 					'error'=>$e->getMessage())
 			);
 		}
 	}
-
 ?>
