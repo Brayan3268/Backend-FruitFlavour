67803b231028ebbfd371d3d51992ce7a09e11e41
Brayan L <brayanlujan.mzu@hotmail.com>
2022-04-27 23:59:51 -0500

    Added the page for delete a user

---
+++ userff_DELETE.php
@@ -0,0 +1,31 @@
+<?php
+	require 'SQLGlobal.php';
+
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		try{
+			$datos = json_decode(file_get_contents("php://input"),true);
+
+            $idCard = $datos["idCard"]; // obtener parametros POST
+
+			$respuesta = SQLGlobal::cudFiltro(
+				"delete from userff where idCard = ?;",
+				array($email)
+			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+			echo json_encode(array(
+				'respuesta'=>'200',
+				'estado' => 'Se eliminó a el usuario de la base de datos correctamente',
+				'data'=>$respuesta,
+				'error'=>''
+			));
+		}catch(PDOException $e){
+			echo json_encode(
+				array(
+					'respuesta'=>'-1',
+					'estado' => 'Ocurrio un error, intentelo mas tarde',
+					'data'=>'',
+					'error'=>$e->getMessage())
+			);
+		}
+	}
+
+?>
